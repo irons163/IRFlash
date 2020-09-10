adbe8b01ed1606bd5ddb871d4ac8eb2204a034c1
Your Name <you@example.com>
2020-09-10 11:26:20 +0800

    Initial Commit

---
+++ IRFlash/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IRFlash
+//
+//  Created by irons on 2020/9/10.
+//  Copyright © 2020 irons163. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ IRFlash/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  IRFlash
+//
+//  Created by irons on 2020/9/10.
+//  Copyright © 2020 irons163. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
